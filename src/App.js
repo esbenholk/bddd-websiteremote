02155fb49129728e67f08059b3136488af10e43b
esben holk nielsen <esbenholknielsen@gmail.com>
2022-05-25 17:50:53 +0200

    initial set up: post request test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
+import {useEffect, useRef, useState} from "react"
 
 function App() {
+
+  const inputField = useRef();
+  const [text, setText] = useState("");
+
+  function sendData(event){
+    event.preventDefault();
+        // POST request using axios inside useEffect React hook
+    console.log("state text", text);
+    const article = { title: text };
+    axios.post('/', article)
+        .then((response) => {
+          console.log("sending data");
+
+          console.log(response);
+        }
+      ).catch((error)=>{
+        console.log("receives no response");
+      });
+
+    setText("keep going... ")
+  }
+
+  function handleChange(event){
+    setText(event.target.value)
+
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>welcome user</h1>
+   
+
+      <form onSubmit={sendData}>
+    
+        <input type="text" value={text} onChange={handleChange} />
+        <button type="submit" value="Submit">submit</button>
+      </form>
       </header>
     </div>
   );
